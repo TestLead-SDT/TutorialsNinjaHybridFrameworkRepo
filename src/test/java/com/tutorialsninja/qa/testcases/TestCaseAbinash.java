f93d08503b148ed7b045f52edf9f8cbb1d4e0324
adgulu96 <das.abinash07@gmail.com>
2023-03-20 16:44:51 +0530

    Hi TeamLead i have done some updates in the framework please check...

---
+++ src/test/java/com/tutorialsninja/qa/testcases/TestCaseAbinash.java
@@ -0,0 +1,12 @@
+package com.tutorialsninja.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCaseAbinash 
+{
+	@Test
+	public void testDemo()
+	{
+		System.out.println("Testlead i have update my work please check");
+	}
+}
